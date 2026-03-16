c460ae9816125a700624731630af3be94a8aa68b
EfraimGottlieb <efgo583208979@gmail.com>
2026-03-16 10:26:24 +0200

    adedd id random generator

---
+++ server/utils/idGenerator.js
@@ -0,0 +1,12 @@
+export const generateId = () => {
+  const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  let autoId = ''
+
+  for (let i = 0; i < 6; i++) {
+    autoId += CHARS.charAt(
+      Math.floor(Math.random() * CHARS.length)
+    )
+  }
+  return autoId
+}
