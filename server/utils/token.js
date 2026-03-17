3d3bf8d8749358ff00326dd133f9492b8791028f
EfraimGottlieb <efgo583208979@gmail.com>
2026-03-17 09:40:05 +0200

    added token utils file

---
+++ server/utils/token.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export function generateToken(payload) {
+  return jwt.sign(payload, process.env.SECRET_KEY);
+}
+
+export function verifyToken(token) {
+  try {
+    return jwt.verify(token, process.env.SECRET_KEY);
+  } catch {
+    return false
+  }
+}
